2a19ade452f68e60411dfec32ca73418d8b77a2d
Junseok Oh <kotohana5706@edcan.kr>
2016-08-09 21:59:57 +0900

    Implement DoubleTextView Implement DoubleTextView Implement DoubleTextView_Attrs Implement DoubleTextView Layout Example

---
+++ app/src/main/java/kr/edcan/alime/views/DoubleTextView.java
@@ -0,0 +1,76 @@
+package kr.edcan.alime.views;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import kr.edcan.alime.R;
+
+/**
+ * Created by JunseokOh on 2016. 8. 9..
+ */
+public class DoubleTextView extends LinearLayout {
+
+    Context c;
+    String primaryText, subText;
+    int primaryColor, subColor;
+    float mainTextSize, subTextSize;
+    TextView mainTextView, subTextView;
+    LayoutParams subParam;
+    Resources res;
+
+    public DoubleTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.c = context;
+        setOrientation(LinearLayout.VERTICAL);
+        setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+        res = context.getResources();
+        setView();
+        getAttrs(attrs);
+    }
+
+    private void getAttrs(AttributeSet attrs) {
+        TypedArray array = getContext().obtainStyledAttributes(attrs, R.styleable.CartaTagView);
+        setTypedArray(array);
+    }
+
+    private void setTypedArray(TypedArray array) {
+        primaryText = array.getString(R.styleable.DoubleTextView_mainText);
+        subText = array.getString(R.styleable.DoubleTextView_subText);
+        primaryColor = array.getInt(R.styleable.DoubleTextView_mainColor, res.getColor(R.color.notSelectedTextColor));
+        subColor = array.getInt(R.styleable.DoubleTextView_subColor, Color.WHITE);
+        mainTextSize = array.getDimension(R.styleable.DoubleTextView_mainTextSize, 18);
+        subTextSize = array.getDimension(R.styleable.DoubleTextView_subTextSize, 25);
+
+        mainTextView.setText(primaryText);
+        mainTextView.setTextColor(primaryColor);
+        mainTextView.setTextSize(mainTextSize);
+        subTextView.setText(subText);
+        subTextView.setTextColor(subColor);
+        subTextView.setTextSize(subTextSize);
+        array.recycle();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        setView();
+        super.onDraw(canvas);
+    }
+
+    private void setView() {
+        mainTextView = new TextView(c);
+        subTextView = new TextView(c);
+        mainTextView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        subParam = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        subParam.setMargins(0, 5, 0, 0);
+        subTextView.setLayoutParams(subParam);
+        addView(mainTextView);
+        addView(subTextView);
+    }
+}

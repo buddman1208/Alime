820d452be256de8d9634fee2c685774dac3ab4ab
Junseok Oh <kotohana5706@edcan.kr>
2016-08-13 18:50:58 +0900

    SkillPageParser: Refactoring

--- app/src/main/java/kr/edcan/alime/utils/SkillPageParser.java
+++ app/src/main/java/kr/edcan/alime/utils/SkillPageParser.java
@@ -1,6 +1,7 @@
 package kr.edcan.alime.utils;
 
 import android.os.AsyncTask;
+import android.support.v4.util.Pair;
 import android.util.Log;
 
 import org.jsoup.Jsoup;
@@ -9,6 +10,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Created by JunseokOh on 2016. 8. 12..
@@ -18,34 +21,57 @@ public class SkillPageParser {
     final private String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36 OPR/39.0.2256.48";
     final private String CSS_QUERY = "td>p>nobr>a";
     final private int TIMEOUT = 5000;
-    public String url = "http://skill.hrdkorea.or.kr/information/in001.action?&&page=";
-    private int currentPage = 0;
+    final static public String defaultUrl= "http://skill.hrdkorea.or.kr/information/in001.action?&&page=";
     private Document skillNotice;
 
-    public SkillPageParser() {
-        currentPage = 1;
+    public SkillPageParser(String documentUrl) {
     }
 
-    class NetworkParsingTask extends AsyncTask<String, Void, String> {1
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-                skillNotice = Jsoup.connect(url + currentPage)
-                        .userAgent(USER_AGENT)
-                        .timeout(TIMEOUT)
-                        .get();
-                Elements elements = skillNotice.select(CSS_QUERY);
-                for (Element e : elements) {
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-//            return title;
+    public ArrayList<PageList> getNoticeList() {
+        ArrayList<PageList> arrayList = new ArrayList<>();
+
+    };
+
+    class PageList {
+        private String title, href, date;
+
+        public PageList(String title, String href, String date) {
+            this.title = title;
+            this.href = href;
+            this.date = date;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getHref() {
+            return href;
+        }
+
+        public String getDate() {
+            return date;
+        }
+    }
+
+    class DocumentGetter extends AsyncTask<String, Void, Document> {
+        private String URL= "";
+        public void setLink(String url) {
+            this.URL = url;
         }
 
         @Override
-        protected void onPostExecute(String aVoid) {
-            super.onPostExecute(aVoid);
+        protected Document doInBackground(String... params) {
+            try {
+                Document doc = Jsoup.connect(url + currentPage)
+                        .userAgent(USER_AGENT)
+                        .timeout(TIMEOUT)
+                        .get();
+                return doc;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
         }
     }
 }

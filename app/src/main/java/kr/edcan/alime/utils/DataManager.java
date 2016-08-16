af317593863c24b234c4bea69d18a64a0cc2be2b
Junseok Oh <kotohana5706@edcan.kr>
2016-08-16 16:52:52 +0900

    NetworkHelper: Configure Retrofit Instance

--- app/src/main/java/kr/edcan/alime/utils/DataManager.java
+++ app/src/main/java/kr/edcan/alime/utils/DataManager.java
@@ -1,8 +1,15 @@
 package kr.edcan.alime.utils;
 
+import android.content.Context;
+
 /**
  * Created by JunseokOh on 2016. 8. 16..
  */
 public class DataManager {
 
+    Context c;
+
+    public DataManager(Context c) {
+        this.c = c;
+    }
 }
--- app/src/main/java/kr/edcan/alime/utils/NetworkHelper.java
+++ app/src/main/java/kr/edcan/alime/utils/NetworkHelper.java
@@ -1,7 +1,40 @@
 package kr.edcan.alime.utils;
 
-/**
- * Created by JunseokOh on 2016. 8. 16..
+/*
+ * Created by Junseok Oh on 2016.
+ * Copyright by Good-Reserve Project by Sunrin Internet High School EDCAN / IWOP / ZEROPEN
+ * All rights reversed.
  */
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+
 public class NetworkHelper {
+    private Context context;
+    final public static String url = "http://kafuuchino.one";
+    final public static int port = 2000;
+
+    public NetworkHelper(Context context) {
+        this.context = context;
+    }
+
+    public static Retrofit retrofit;
+
+    public static NetworkInterface getNetworkInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(url + ":" + port)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(NetworkInterface.class);
+    }
+
+    public static boolean returnNetworkState(Context context) {
+        final ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
 }
